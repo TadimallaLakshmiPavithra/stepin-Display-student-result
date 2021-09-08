610a5e1c0e69957692a855749db7ce6325461822
Git_username <Git_user_email>
2021-09-08 15:46:36 +0530

    update

---
+++ 3_Implementation/src/func.h
@@ -0,0 +1,15 @@
+#ifndef _FUNC_H_
+#define _FUNC_H_
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+
+
+int password();
+void addrecord();
+void viewrecord();
+void editrecord();
+void editpassword();
+void deleterecord();
+#endif
